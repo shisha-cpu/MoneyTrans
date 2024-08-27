85ab4e02055c251d801d6a093c6cb4ab9c21f85e
salamus <salamus@yandex.com>
2024-08-28 02:17:26 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Пример простого скрипта для плавного скроллинга при клике на навигацию
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
